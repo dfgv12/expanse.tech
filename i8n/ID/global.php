bcd849edee6d452d19a679a476b404ed9bfe09cc
dfgv12 <dhimas_pambudi@yahoo.com>
2016-07-03 22:03:35 +0700

    Global ID version
    Id version

---
+++ i8n/ID/global.php
@@ -0,0 +1,17 @@
+<?php
+//Expanse Internationalzation Language File - English : Global 
+//Version: 0.0.1
+
+//Global : General
+$i8n['global_title']='Expanse | Blockchain Platform Terbuka';
+
+// Global : Navigation
+$i8n['nav_screenread']='Alihkan Navigasin';
+$i8n['nav_slogan']='Ini\' Untuk siapa saja.';
+$i8n['nav_about']='Tentang';
+$i8n['nav_downloads']='Download';
+$i8n['nav_documentation']='Dokumentasi';
+$i8n['nav_team']='Tim';
+$i8n['nav_community']='Komunitas';
+$i8n['nav_explorer']='Block Explorer';
+?>
