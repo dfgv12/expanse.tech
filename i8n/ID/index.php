c0e66e9f54e5e4e5ae43b2111d8cbe5150378b9b
dfgv12 <dhimas_pambudi@yahoo.com>
2016-07-03 22:25:06 +0700

    Index Indonesian Version
    Indonesian Version

---
+++ i8n/ID/index.php
@@ -0,0 +1,84 @@
+<?php
+//Expanse Internationalzation Language File - English : Global 
+//Version: 0.0.1
+
+//Main Page : Heading
+$i8n['main_headline']='Komunitas didorong <br />Desentralisasi platform blockchain';
+$i8n['main_description']='Expanse adalah informasi desentralisasi kriptografi, aplikasi, dan platform kontrak. Proyek ini dioperasikan oleh penggunanya melalui adil, terbuka, masyarakat yang dikelola organisasi terdesentralisasi.';
+$i8n['main_readmore']='Baca lebih';
+
+//Main Page : About
+$i8n['main_about_what']='Apa itu Expanse?';
+$i8n['main_about_left']='Expanse adalah sebuah komunitas didorong, informasi desentralisasi, aplikasi, dan platform kontrak cerdas. Proyek Bentangan dioperasikan oleh komunitasnya melalui DAO (desentralisasi organisasi otonom) di mana anggotanya langsung berkolaborasi; bertukar pikiran dan ide-ide; dan suara untuk menentukan proyek  perkembangan masa depan dan keputusan manajemen.';
+$i8n['main_about_right']='Menampilkan koleksi perkembangan kontrak cerdas, dApps dan toolkit menyediakan pengguna dan organisasi dari semua jenis dengan akses intuitif untuk beberapa aplikasi yang paling menarik dari teknologi blockchain. Proyek ini menyediakan alat dan layanan yang bisnis, badan amal, bahkan pemerintah dapat digunakan untuk dengan mudah dan intuitif memanfaatkan teknologi blockchain dari menciptakan dan mengelola aset cerdas untuk memberikan pemenuhan digital dan distribusi.';
+$i8n['main_about_title1']='Kontrak pintar & DApps';
+$i8n['main_about_desc1']='Beragam, dinamis, desentralisasi aplikasi yang berjalan di hamparan Blockchain. Dari pasar desentralisasi, pendaftar global, komputasi ditegakkan perjanjian, untuk seluruh organisasi dioperasikan secara eksklusif pada blockchain tersebut.';
+$i8n['main_about_title2']='Semua terdesentralisasi.';
+$i8n['main_about_desc2']='Desentralisasi Penyimpanan Data, Penyimpanan Catatan, Informasi Pengolahan, Smart Aset, dan banyak lagi.
+Hamparan memungkinkan untuk dunia inovasi yang dibangun di atas teknologi didistribusikan....';
+$i8n['main_about_title3']='Cepat. Intuitif. Kuat.';
+$i8n['main_about_desc3']='teknologi Blockchain memenuhi Kontrak pintar komplex untuk membawa Anda hasil belum pernah terjadi sebelumnya. Eksponensial meningkatkan kecepatan, kehandalan, dan kinerja yang dibuat tersedia untuk secara drastis mengurangi biaya bila dibandingkan dengan solusi tradisional.';
+$i8n['main_about_title4']='KomunitasExpanse';
+$i8n['main_about_desc4']='Proyek Expanse ini dikelola oleh sebuah organisasi yang terdesentralisasi yang beroperasi pada hamparan Blockchain. entitas ini bertanggung jawab untuk keputusan yang signifikan seperti memutuskan apa fitur atau pembaruan yang akan difokuskan oleh pengembang.';
+
+//Main Page : Downloads
+$i8n['main_downloads_header']='Download';
+$i8n['main_downloads_gexptitle']='gexp - implementasi referensi inti';
+$i8n['main_downloads_gexpdesc']='Tujuan pelaksanaan Exanse. Dikembangkan di GO, Ini adalah cara yang ideal untuk mengoperasikan sebuah simpul Expanse atau sebagai backend untuk bentangan Terdistribusi Aplikasi.';
+$i8n['main_downloads_wallettitle']='expwallet graffic kekayaan wallet Expanse';
+$i8n['main_downloads_walletdesc']='Mudah intuitif, menggunakan klien menampilkan display rekening dan transaksi, memungkinkan untuk akses langsung ke fitur yang paling digunakan di hamparan.';
+$i8n['main_downloads_soon']='Segera hadir';
+$i8n['main_downloads_download']='Download';
+$i8n['main_downloads_close']='Tutup';
+
+//Main Page : Explore
+$i8n['main_explore_header']='<strong>Explore</strong> Dunia baru;
+
+//Main Page : Documentation
+$i8n['main_documentation_header']='Dokumentasi';
+$i8n['main_documentation_title']='Anda harus tahu';
+$i8n['main_documentation_desc']='Pelajari lebih lanjut tentang proyek Hamparan dan teknologi dan \'s. Dari panduan cepat untuk merek pengguna baru dengan spesifikasi teknis yang rinci, array dokumentasi tersedia untuk membantu Anda menggunakan dan memahami hamparan.';
+$i8n['main_documentation_start']='Ayo mulai';
+$i8n['main_documentation_install']='Instalasi';
+$i8n['main_documentation_concepts']='Konsep dasar';
+$i8n['main_documentation_backup']='Back Up &amp; Restoring';
+$i8n['main_documentation_mining']='Mulai pertambangan dengan cepat';
+$i8n['main_documentation_accounts']='Mengatur akun';
+$i8n['main_documentation_tx']='Mengirim transaksi';
+$i8n['main_documentation_tech']='Dokumentasi teknis';
+$i8n['main_documentation_guide']='Panduan pengguna';
+$i8n['main_documentation_roadmap']='Roadmap';
+$i8n['main_documentation_whitepaper']='Whitepaper';
+
+//Main Page : Team
+$i8n['main_team_heading']='bertemu dengan tim';
+$i8n['main_team_title_chris']='Pemimpin developer';
+$i8n['main_team_desc_chris']='Pengembang cryptocurrency berpengalaman dengan pengetahuan tingkat ahli teknologi blockchain, Chris memiliki pemahaman yang beragam industri, dan reputasi berdiri lama di masyarakat.';
+$i8n['main_team_title_dan']='Co-Developer';
+$i8n['main_team_desc_dan']='Dan adalah pendukung teknologi blockchain dan pengembang, memberikan konsultasi untuk sejumlah proyek dan perusahaan di ruang cryptocurrency dan blockchain teknologi.';
+$i8n['main_team_title_james']='Community Manager';
+$i8n['main_team_desc_james']='James adalah seorang analis kripto mata uang, investor, dan antusias yang merupakan pendiri salah satu komunitas yang aktif terbesar untuk Cryptocurrency diskusi di dunia. Seorang manajer komunitas berpengalaman, James melayani di tim beberapa proyek teknologi blockchain mapan.';
+$i8n['main_team_title_marcia']='Strategi Pemasaran';
+$i8n['main_team_desc_marcia']='Marcia Danzeisen Melakukan pemasaran dan strategi untuk beberapa perusahaan bank dan jasa keuangan teknologi terbesar di dunia. Seorang penulis, Danzeisen melihat bagaimana mata uang kripto dan teknologi blockchain telah mengubah dunia FinTech tradisional dan menceburkan diri ke dalam ruang.';
+$i8n['main_team_title_esias']='Int. Pengembangan Bisnis Cina';
+$i8n['main_team_desc_esias']='Yesaya adalah penghubung bisnis yang berpengalaman yang mengkhususkan diri di pasar global. Sebagai mantan Operasi VP di Cryptoexchange Inc., ia memiliki pengalaman industri yang kuat memuji latar belakang dalam perangkat lunak dan pengembangan bisnis.';
+$i8n['main_team_title_tim']='Int. Pengembangan bisnis Perancis';
+$i8n['main_team_desc_tim']='Timothée adalah berpendidikan diri antusias kripto. Dia mengkustomisasi dan eksploitasi wavelet analisis untuk Global Finance & Cryptocurrencies pemrosesan sinyal. Mantan moderator di BTCe dan sekarang berpartisipasi dalam pertemuan kripto di Bordeaux, Prancis.';
+$i8n['main_team_title_sandro']='Art Director';
+$i8n['main_team_desc_sandro']='Sandro telah bekerja dengan beberapa perusahaan blockchain dari seluruh dunia. Dia terampil dengan konsep visual dan desain. Seorang ahli menciptakan interface, website, gerak grafis, 3D, dan seni kreatif.';
+$i8n['main_team_title_dave']='Official Forum Guru';
+$i8n['main_team_desc_dave']='Tertarik pada teknologi dan penyebaran perangkat lunak masyarakat berkembang, Dave adalah masyarakat dan blockchain antusias dengan pengalaman bertahun-tahun di forum dan moderasi masyarakat.';
+$i8n['main_team_title_nathan']='Developer';
+$i8n['main_team_desc_nathan']='Seorang programmer otodidak dari Sydney Australia yang suka bahasa pemrograman yang paling, favorit nya menjadi Node atau Javascript. Nathan telah bekerja pada beberapa proyek kripto lain di masa lalu, dan pengembang pada proyek MPOS, dan membantu dengan stratum (python) pemrograman.';
+//Main Page : Community 
+$i8n['main_community_heading']='Komunitas';
+$i8n['main_community_title']='Forum Komunitas';
+$i8n['main_community_desc']='Expanse <a href="//forum.expanse.tech"><span class="link_white">Forum</span></a> adalah tempat terbaik untuk diskusi rinci Expanse dan percakapan bersejarah mengenai proyek.';
+$i8n['main_community_featured']='Sub-Forums unggulan';
+$i8n['main_community_subforum1']='Berita &amp; Pengumuman';
+$i8n['main_community_subforum2']='Pengembangan dan Diskusi Teknis';
+$i8n['main_community_subforum3']='Kontrak &amp; Desentralisasi aplikasi';
+$i8n['main_community_interact']='Inraksi secara langsung';
+$i8n['main_community_interact_desc']='masyarakat yang paling aktif di slack. Ini adalah tempat yang disarankan jika Anda ingin berpartisipasi atau mendiskusikan hamparan secara real time dengan masyarakat dan pengembang.';
+$i8n['main_community_social']='Sosial Media &amp; Blog';
+$i8n['main_community_blog']='BLOG EXPANSE';
